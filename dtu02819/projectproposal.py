8dc78d199e89bdbf8ea9438f6eb02ae1e079465b
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-10-16 18:16:49 +0200

    pylint, flake8 and pep257 conformance.

--- dtu02819/projectproposal.py
+++ dtu02819/projectproposal.py
@@ -1,17 +1,20 @@
 """Handle projectproposals.
 
+Students hand in project proposals in a predefined JSON format where
+they specify title, a list of group members, git repository,
+description and a list of modules.
 
 Example
 -------
 >>> JSON = '''
 {
   "title": "Data mining of Data Mining using Python",
-  "members": [ 
-    { "name": "Finn Aarup Nielsen", 
+  "members": [
+    { "name": "Finn Aarup Nielsen",
       "study_number": "faan"
     }
   ],
-  "git": "git@github.com:fnielsen/dtu02819.git", 
+  "git": "git@github.com:fnielsen/dtu02819.git",
   "description": "Data mining of data from the DTU course 02819",
   "modules": ["nltk", "csv", "django", "pandas", "gensim", "sklearn"]
 }'''
@@ -29,12 +32,20 @@ import re
 FILENAME_PROJECTPROPOSAL = 'projectproposal.json'
 
 
-
 class ProjectProposal(object):
+
     """Represent one projectproposal."""
 
     def __init__(self, filename=FILENAME_PROJECTPROPOSAL):
-        self.fields = ['title', 'members', 'git', 'description', 'modules']
+        """Read and check project proposal from file or directory name."""
+        self.fields = ['title', 'members', 'git', 'description',
+                       'modules']
+        self.title = ""
+        self.members = []
+        self.git = ""
+        self.description = ""
+        self.modules = []
+        self.raw = ""
         if os.path.isfile(filename):
             self.read(filename)
         elif os.path.isdir(filename):
@@ -57,8 +68,8 @@ class ProjectProposal(object):
 
     def read(self, filename):
         """Set fields from JSON file."""
-        with codecs.open(filename, encoding='utf-8') as f:
-            self.raw = f.read()
+        with codecs.open(filename, encoding='utf-8') as fid:
+            self.raw = fid.read()
         data = json.loads(self.raw)
         for field in self.fields:
             setattr(self, field, data[field])
@@ -82,57 +93,65 @@ class ProjectProposal(object):
             self.modules = re.findall('\W+', self.modules, flags=re.UNICODE)
 
     def keys(self):
+        """Return project proposal attributes as keys."""
         return self.fields
 
     def items(self):
+        """Return project proposal attributes and values as tuples."""
         return [(key, getattr(self, key)) for key in self.keys()]
 
     def values(self):
+        """Return project proposal attributes values."""
         return [getattr(self, key) for key in self.keys()]
-    
+
 
 class ProjectProposals(object):
+
     """Represent a list of proposals."""
-    
+
     def __init__(self, dirname='.'):
+        """Initialize and read project proposals under a given directory."""
         self._proposals = []
         self.read(dirname=dirname)
 
     def read(self, dirname):
+        """Read all project proposal files under a given directory."""
         subdirnames = os.listdir(dirname)
         for subdirname in subdirnames:
-            if re.match('s\d+', subdirname ) or '(faan)' == subdirname:
+            if re.match('s\d+', subdirname) or '(faan)' == subdirname:
                 proposal = {'uploader': subdirname, 'status': 'ok'}
                 try:
                     proposal.update(ProjectProposal(subdirname).items())
-                except (KeyError, ValueError) as e:
-                    proposal['status'] = str(e)
+                except (KeyError, ValueError) as err:
+                    proposal['status'] = str(err)
                 self._proposals.append(proposal)
 
     def get_titles(self):
         """Return all titles."""
         return [proposal.get('title', '') for proposal in self._proposals]
-            
-    def to_status_df(self, columns=['uploader', 'title', 'status']):
+
+    def to_status_df(self, columns=('uploader', 'title', 'status')):
         """Convert proposals to dataframe with status."""
-        return pd.DataFrame([[proposal.get(column, '') for column in columns] 
+        return pd.DataFrame([[proposal.get(column, '') for column in columns]
                              for proposal in self._proposals], columns=columns)
-                                       
+
     def to_modules_df(self):
-        """Convert module description to dataframe."""
-        # Columns and rows        
+        """Convert module description to Pandas dataframe."""
+        # Columns and rows
         modules = set()
         for proposal in self._proposals:
             modules |= set(proposal.get('modules', []))
         index = [proposal['uploader'] for proposal in self._proposals]
 
         # Set elements of dataframe
-        df = pd.DataFrame(index=index, columns=modules)
+        dataframe = pd.DataFrame(index=index, columns=modules)
         for proposal in self._proposals:
-            for module in df.columns:
-                df.ix[proposal['uploader'], module] = module in proposal.get('modules', [])
-        return df
-        
+            for module in dataframe.columns:
+                dataframe.ix[proposal['uploader'], module] = \
+                    module in proposal.get('modules', [])
+        return dataframe
+
     def to_html(self):
-        return (self.to_status_df().to_html() + '\n<br><br>\n' + 
+        """Convert data to an HTML string."""
+        return (self.to_status_df().to_html() + '\n<br><br>\n' +
                 self.to_modules_df().to_html())

a97d7b820a395dfeca7e1c466bc4d3b3e51fd1a7
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-10-28 19:19:25 +0100

    Draft.

---
+++ dtu02819/test/test_projectproposals.py
@@ -0,0 +1,14 @@
+
+
+JSON = """
+{
+  "title": "Data mining of Data Mining using Python",
+  "members": [
+    { "name": "Finn Aarup Nielsen",
+      "study_number": "faan"
+    }
+  ],
+  "git": "git@github.com:fnielsen/dtu02819.git",
+  "description": "Data mining of data from the DTU course 02819",
+  "modules": ["nltk", "csv", "django", "pandas", "gensim", "sklearn"]
+}"""

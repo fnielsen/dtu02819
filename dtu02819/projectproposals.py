bc706a71993e110070476334ae7b698eed6ed0d4
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-10-28 19:20:03 +0100

    Removed example to test function because doctest did not like it.

--- dtu02819/projectproposals.py
+++ dtu02819/projectproposals.py
@@ -4,21 +4,6 @@ Students hand in project proposals in a predefined JSON format where
 they specify title, a list of group members, git repository,
 description and a list of modules.
 
-Example
--------
->>> JSON = '''
-{
-  "title": "Data mining of Data Mining using Python",
-  "members": [
-    { "name": "Finn Aarup Nielsen",
-      "study_number": "faan"
-    }
-  ],
-  "git": "git@github.com:fnielsen/dtu02819.git",
-  "description": "Data mining of data from the DTU course 02819",
-  "modules": ["nltk", "csv", "django", "pandas", "gensim", "sklearn"]
-}'''
-
 """
 
 

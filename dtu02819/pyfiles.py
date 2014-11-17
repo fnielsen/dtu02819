07d865bcfacfdc688cb955472188ec62df7995a9
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-11-17 09:32:11 +0100

    Changed to used properties.

--- dtu02819/pyfiles.py
+++ dtu02819/pyfiles.py
@@ -1,6 +1,10 @@
 """Perform various checks on Python files."""
 
 
+from __future__ import unicode_literals
+
+from lazy import lazy
+from operator import attrgetter
 import os
 import pandas as pd
 import pep257
@@ -15,6 +19,10 @@ class CheckError(Exception):
     pass
 
 
+def attr_to_key(attrname):
+    return re.sub('_', ' ', attrname)
+
+
 class PyFile(dict):
 
     """Representation of a Python file.
@@ -25,19 +33,18 @@ class PyFile(dict):
     >>> 'pyfiles.py' in pyfile['Filename']
     True
 
-    >>> pyfile.get_pylint_rating() > 9.5
+    >>> pyfile.pylint_rating() > 9.5
     True
 
     """
 
     def __init__(self, filename, check_all=True):
         dict.__init__(self)
-        self['Filename'] = filename
+        self['filename'] = filename
         self._pattern_pylint_rating = \
             re.compile(r'Your code has been rated at (\d+\.\d{2})')
-        if check_all:
-            self.check_all()
-
+        self.attr_to_dict()
+        
     def run_pep257(self):
         """Run and return output from pep257 tool."""
         return pep257.check([self['Filename']])
@@ -53,7 +60,14 @@ class PyFile(dict):
                 raise CheckError(msg)
         return pylint_stdout.read()
 
-    def get_pylint_rating(self):
+    def attr_to_dict(self):
+        attrs = ['number_of_lines', 'pylint_rating', 
+                 'number_of_pep257_issues']
+        for attr in attrs:
+            self[attr_to_key(attr)] = attrgetter(attr)(self)
+ 
+    @lazy
+    def pylint_rating(self):
         """Get the pylint rating for the file."""
         output = self.run_pylint()
         matches = self._pattern_pylint_rating.findall(output)
@@ -64,19 +78,15 @@ class PyFile(dict):
         else:
             raise CheckError('Did not match pylint rating')
 
-    def get_number_of_lines(self):
+    @lazy
+    def number_of_lines(self):
         """Return total number of lines in file."""
         return len(open(self['Filename']).read().strip().split('\n'))
 
-    def get_number_of_pep257_issues(self):
+    @lazy
+    def number_of_pep257_issues(self):
         """Return number of issues that the pepe257 reports."""
         return len(list(self.run_pep257()))
-
-    def check_all(self):
-        """Compute all characteristics of a file."""
-        self['Number of lines'] = self.get_number_of_lines()
-        self['Pylint rating'] = self.get_pylint_rating()
-        self['Number of pep257 issues'] = self.get_number_of_pep257_issues()
 
     def to_series(self):
         """Return data about Python file as Pandas Series."""
@@ -87,9 +97,12 @@ class PyFiles(list):
 
     """Represent data about a set of Python files in a list."""
 
-    def __init__(self, dirname='.'):
+    def __init__(self, dirname='.', ignore=None):
+        """Read py-files and evaluate them."""
         list.__init__(self)
         self.top_dir = dirname
+        if ignore is None:
+            self.ignore = set(['.tox'])
         self.read_py_files()
 
     @staticmethod
@@ -107,9 +120,11 @@ class PyFiles(list):
     def py_filenames(self):
         """Return generator for .py files by walking directories."""
         for dirpath, _, filenames in os.walk(self.top_dir):
+            # TODO should also ignore dirs matching ignore.
             for filename in filenames:
                 filename_with_path = os.path.join(dirpath, filename)
-                if self.is_py_filename(filename_with_path):
+                if self.is_py_filename(filename_with_path) and \
+                        filename not in self.ignore:
                     yield filename_with_path
 
     def read_py_files(self):
@@ -117,26 +132,29 @@ class PyFiles(list):
         for py_filename in self.py_filenames():
             self.append(PyFile(py_filename))
 
+    @lazy
     def total_number_of_lines(self):
         """Return the sum of the number of lines."""
-        return sum([pyfile['Number of lines'] for pyfile in self])
+        return sum([pyfile.number_of_lines for pyfile in self])
 
+    @lazy
     def min_pylint_rating(self):
         """Return the minimum pylint rating across files."""
-        return min([pyfile['Pylint rating'] for pyfile in self])
+        return min([pyfile.pylint_rating for pyfile in self])
 
+    @lazy
     def max_number_of_pep257_issues(self):
         """Return the maximum number of pep257 issues across files."""
-        return max([pyfile['Number of pep257 issues'] for pyfile in self])
+        return max([pyfile.number_of_pep257_issues for pyfile in self])
 
     def to_df(self):
         """Convert data to Pandas DataFrame."""
         return pd.DataFrame(self)
 
     def to_series(self):
-        results = {
-            'Total number of lines': self.total_number_of_lines(),
-            'Min pylint rating': self.min_pylint_rating(),
-            'Max number of pep257 issues': self.max_number_of_pep257_issues()
-            }
+        """Convert overall results to Pandas Series."""
+        attrs = ['total_number_of_lines', 'min_pylint_rating',
+                 'max_number_of_pep257_issues']
+        results = {attr_to_key(attr): attrgetter(attr)(self)
+                   for attr in attrs}
         return pd.Series(results)
